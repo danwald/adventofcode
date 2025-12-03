b6e7699906673308617937ebc2b1569dbea91f20
danny crasto <danwald79@gmail.com>
2025-12-03 22:42:24 +0400

    2025 01 initial commit WIP
    doesn't work for default testcase

---
+++ 2025/01_secret_entrance.py
@@ -0,0 +1,53 @@
+import sys
+
+from collections import namedtuple
+
+Heading = namedtuple("Heading", ["dr", "mg"])
+
+
+class Dial:
+    def __init__(self, teeth: int = 99):
+        self.teeth = teeth
+        self.pos = 0
+        self._pass = 0
+
+    def __len__(self) -> int:
+        return self.teeth
+
+    @property
+    def password(self) -> int:
+        return self._pass
+
+    def _how(self, line: str) -> Heading:
+        ln = line.strip()
+        d, m = ln[0], ln[1:]
+        return Heading(-1 if d == "L" else 1, int(m))
+
+    def rotate(self, line: str) -> None:
+        heading = self._how(line)
+        rots, inc = divmod(len(self), heading.mg)
+        self._pass += rots
+        if heading.dr < 0:
+            self.pos -= inc
+            if self.pos < 0:
+                self.pos = len(self) - self.pos - 1
+                self._pass += 1
+        else:
+            self.pos += inc
+            if self.pos > len(self):
+                self.pos = self.pos - len(self) - 1
+                self._pass += 1
+
+        print(line, self.pos)
+
+
+def main():
+    dl = Dial()
+    while line := sys.stdin.readline().strip():
+        dl.rotate(line)
+    return dl.password, dl.pos
+
+
+if __name__ == "__main__":
+
+    print(main())
