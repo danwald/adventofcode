a108f88fba94b3aa480a91e5dc5d2f8aaa0f6955
danny crasto <danwald79@gmail.com>
2025-12-05 07:22:50 +0400

    day 1 works for first puzzle

--- 2025/01_secret_entrance.py
+++ 2025/01_secret_entrance.py
@@ -2,14 +2,14 @@ import sys
 
 from collections import namedtuple
 
-Heading = namedtuple("Heading", ["dr", "mag"])
+Heading = namedtuple("Heading", ["dr", "dt"])
 
 
 class Dial:
     def __init__(self, pos: int = 50, teeth: int = 100):
         self.teeth = teeth
-        self.pos = pos
         self._pass = 0
+        self.pos = pos
 
     def __len__(self) -> int:
         return self.teeth
@@ -23,31 +23,47 @@ class Dial:
         d, m = ln[0], ln[1:]
         return Heading(-1 if d == "L" else 1, int(m))
 
-    def rotate(self, line: str) -> None:
+    def rotate(self, line: str, pst=False, dbg=False) -> bool:
         heading = self._how(line)
-        rots, inc = divmod(heading.mag, len(self))
-        self._pass += rots
-        if heading.dr < 0:
-            self.pos -= inc
-            if self.pos < 0:
-                self.pos = len(self) + self.pos
-        else:
-            self.pos += inc
-            if self.pos > len(self):
-                self.pos = self.pos - len(self)
+        rots, rdt = divmod(heading.dt, len(self))
+        thru = 0
+        c0 = not self.pos
+        self.pos += heading.dr * rdt
+        if self.pos >= len(self):
+            self.pos -= len(self)
+            if not c0:
+                thru += 1
+        elif self.pos < 0:
+            self.pos += len(self)
+            if not c0:
+                thru += 1
 
-        if self.pos == len(self):
-            self.pos = 0
-        assert 0 <= self.pos < len(self)
+        if not (0 <= self.pos < len(self)):
+            if dbg:
+                print(f"{line}[{self.pos}] R:{rots} = {self.password}")
+            return False
         if not self.pos:
+            if dbg:
+                print(f"{line}[{self.pos}] R:{rots} = {self.password}", end="")
             self._pass += 1
-        print(line, rots, inc, self.pos, self.password)
+            if dbg:
+                print(f"->{self.password}")
+
+        if pst:
+            if dbg:
+                print(f"{line}[{self.pos}] R:{rots} = {self.password}", end="")
+            self._pass += rots + thru
+            if dbg:
+                print(f"-> {self.password}")
+
+        return True
 
 
 def main():
     dl = Dial()
     while line := sys.stdin.readline().strip():
-        dl.rotate(line)
+        if not dl.rotate(line, False):
+            print(f"failed at {line}, {dl.password}")
     return dl.password, dl.pos
 
 
